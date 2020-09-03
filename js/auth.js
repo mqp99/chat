e70fb8c09684db9ff54caa7519edc40f1b592240
mqp99 <m99excellent@gmail.com>
2020-09-03 15:27:12 +0700

    update

--- js/auth.js
+++ js/auth.js
@@ -21,43 +21,49 @@ $(function(){
 		$('.auth__box').slideDown();
 	})
 
-	btnSignOut.on('click', function signOutAuth(){
-		firebase.auth()
-			.signOut()
-			.then(() => {
-
-				var profile = {
-					'uid': '',
-					'email': '',
-					'photoUrl': '',
-					'fullname': '',
-					// 'emailVerified': user.emailVerified
-				}
-
-				localStorage.setItem('user-profile', JSON.stringify(profile));
-
-				if(lastPathname == 'chat.html') {
-
-					window.location.href = 'index.html';
-
-				}
-			})
+	// Sign in with Facebook accout
+	btnSignInWithFB.on('click', function signInWithFacebook(){
+		// new Firebase auth
+		var provider = new firebase.auth.FacebookAuthProvider();
+		firebase.auth().signInWithPopup(provider)
+				.then(result => {
+					console.log(result);
+					console.log('Login fb');
+				})
+				.catch(err => { console.log(err); });
 	})
 
+	// Sign in with GOOGLE accout
 	btnSignInWithGG.on('click', function signInWithGoogle(){
-
+		// new Firebase auth
 		var provider = new firebase.auth.GoogleAuthProvider();
+		// open popup login
+		firebase.auth().signInWithPopup(provider).catch(err => { console.log(err); });
+	})
 
-		firebase.auth()
-			.signInWithPopup(provider)
-			.catch(err => {
-				console.log(err);
-			})
+	// Sign out all account
+	btnSignOut.on('click', function signOutAuth(){
+		firebase.auth() .signOut() .then(() => {
+			// create obj
+			var profile = {
+				'uid': '',
+				'email': '',
+				'photoUrl': '',
+				'fullname': '',
+				// 'emailVerified': user.emailVerified
+			}
+			// add obj to json
+			localStorage.setItem('user-profile', JSON.stringify(profile));
+			// redirect to login
+			if(lastPathname == 'chat.html') {
+				window.location.href = 'index.html';
+			}
+		})
 	})
 
 	firebase.auth().onAuthStateChanged(user => {
 		if(user) {
-
+			// create obj
 			var profile = {
 				'uid': user.uid,
 				'email': user.email,
@@ -65,9 +71,9 @@ $(function(){
 				'fullname': user.displayName,
 				// 'emailVerified': user.emailVerified
 			}
-
+			// add obj to json
 			localStorage.setItem('user-profile', JSON.stringify(profile));
-
+			// redirect to chat
 			if(lastPathname == 'index.html' || lastPathname == '') {
 
 				window.location.href = 'chat.html';
